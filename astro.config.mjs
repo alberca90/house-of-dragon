92ca03275a43eaf0fd0648f0307c9838d1e7f97e
alberca90 <alberca.pozo@gmail.com>
2026-08-20 19:12:25 +0200

    fix: add vercel adapter for keystatic ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server', // Permite renderizar el panel /keystatic en producción
+  adapter: vercel(),
   integrations: [
     react(),     // <--- Añadir esta integración
     keystatic(),
