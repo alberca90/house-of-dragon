c2cee141ef541bf80cb62586f4c3314195965e06
alberca90 <alberca.pozo@gmail.com>
2026-08-21 08:49:17 +0200

    fix: eliminar configuración de salida del servidor en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server', // Permite renderizar el panel /keystatic en producción
   adapter: vercel(),
   integrations: [
     react(),     // <--- Añadir esta integración
